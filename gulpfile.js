c8f704b3d3d06d555be38874ee1d541dbb9e1119
heidilaursen <heidimlaursen@gmail.com>
2016-06-29 13:41:20 -0700

    gulp and eslinting added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,33 @@
 const gulp = require('gulp');
-const gulp = require('gulp-eslint');
-const gulp = require('gulp-mocha');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
 
-var files = [];
+var files = ['lib/*', 'bin/*'];
 
-gulp.task('');
+gulp.task('lint:test', () => {
+  return gulp.src('.test/**/*test.js')
+    .pipe(eslint({
+      envs: [
+        'mocha',
+        'es6'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:nontest', () => {
+  return gulp.src(files)
+    .pipe(eslint({
+      envs: [
+        'es6'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task('mocha', () => {
+  return gulp.src('./test/**/*test.js')
+    .pipe(mocha());
+});
+
+gulp.task('default', ['lint:test', 'lint:nontest', 'mocha']);
